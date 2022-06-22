bc47122d11d02ecb811ab8c5a0da3421882695f0
Danil Astakhov <thrixstudio@yandex.ru>
2022-06-22 17:14:41 +0700

    develop: Async is gone

--- src/main.py
+++ src/main.py
@@ -1,11 +1,8 @@
-from telebot.async_telebot import AsyncTeleBot
+from telebot import TeleBot
 from telebot.types import Message
 from os import environ, path
-from asyncio import run
 
-bot = AsyncTeleBot(environ.get("TOKEN"), parse_mode=None)
-
-sticker = open(path.join("assets/", "hi_sticker.webp"), "rb")
+bot = TeleBot(environ.get("TOKEN"), parse_mode=None)
 
 all_content_types = [
     "text",
@@ -34,9 +31,11 @@ all_content_types = [
 
 
 @bot.message_handler(commands=["start", "help"])
-async def start(message: Message):
-    await bot.send_sticker(message.chat.id, sticker)
-    await bot.send_message(
+def start(message: Message):
+    sticker = open(path.join("assets/", "hi_sticker.webp"), "rb")
+    bot.send_sticker(message.chat.id, sticker)
+    sticker.close()
+    bot.send_message(
         message.chat.id,
         "*Пр-р-ривет!*\n\n"
         + "Я - попугай *Кеша*. Я *повторяю* за *тобой* *все* сообщения! *Кар-р!*",
@@ -45,44 +44,40 @@ async def start(message: Message):
 
 
 @bot.message_handler(content_types=["text"])
-async def echo_text(message: Message):
-    await bot.send_message(
+def echo_text(message: Message):
+    bot.send_message(
         message.chat.id, "_" + message.text.strip("_*") + "_", parse_mode="Markdown"
     )
 
 
 @bot.message_handler(content_types=["sticker"])
-async def echo_sticker(message: Message):
-    await bot.send_sticker(message.chat.id, message.sticker.file_id)
+def echo_sticker(message: Message):
+    bot.send_sticker(message.chat.id, message.sticker.file_id)
 
 
 @bot.message_handler(content_types=["animation", "photo", "video", "video_note"])
-async def echo_video(message: Message):
-    await bot.send_message(
-        message.chat.id, "*Выглядит кр-р-руто!*", parse_mode="Markdown"
-    )
+def echo_video(message: Message):
+    bot.send_message(message.chat.id, "*Выглядит кр-р-руто!*", parse_mode="Markdown")
 
 
 @bot.message_handler(content_types=["audio", "voice"])
-async def echo_audio(message: Message):
-    await bot.send_message(
-        message.chat.id, "*Звучит невер-р-роятно!*", parse_mode="Markdown"
-    )
+def echo_audio(message: Message):
+    bot.send_message(message.chat.id, "*Звучит невер-р-роятно!*", parse_mode="Markdown")
 
 
 @bot.message_handler(content_types=["pinned_message"])
-async def echo_pinned(message: Message):
-    await bot.send_message(message.chat.id, "*Закр-р-реплено!*", parse_mode="Markdown")
+def echo_pinned(message: Message):
+    bot.send_message(message.chat.id, "*Закр-р-реплено!*", parse_mode="Markdown")
 
 
 @bot.message_handler(content_types=["new_chat_members"])
-async def echo_new_chat_members(message: Message):
+def echo_new_chat_members(message: Message):
     reply_text = "*Пр-р-ривет"
     if len(message.new_chat_members) == 1:
         reply_text += f", _{message.new_chat_members[0].first_name}_!*"
     else:
         reply_text += " _всем_!*"
-    await bot.send_message(
+    bot.send_message(
         message.chat.id,
         reply_text,
         parse_mode="Markdown",
@@ -90,8 +85,13 @@ async def echo_new_chat_members(message: Message):
 
 
 @bot.message_handler(content_types=all_content_types)
-async def echo_all(message: Message):
-    await bot.send_message(message.chat.id, "*Кар-р-р!*", parse_mode="Markdown")
+def echo_all(message: Message):
+    bot.send_message(message.chat.id, "*Кар-р-р!*", parse_mode="Markdown")
 
 
-run(bot.infinity_polling(allowed_updates=["message"]))
+def main():
+    bot.infinity_polling()
+
+
+if __name__ == "__main__":
+    main()
